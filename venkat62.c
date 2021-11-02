7479550948d650af31774433d990417a2aa330f1
Rajavenkat5 <93177152+Rajavenkat5@users.noreply.github.com>
2021-11-02 12:21:06 +0530

    Add files via upload

---
+++ venkat62.c
@@ -0,0 +1,22 @@
+/* All arithmatic operations */
+#include<stdio.h>
+void main()
+{
+	int a ,b ,s ,d ,p ,q , r;
+	
+	a=11;
+	b=2;
+	
+	s=a+b;
+	d=a-b;
+	p=a*b;
+	q=a/b;
+	r=a%b;
+	
+	printf("sum of %d and %d is %d\n",a,b,s);
+	printf("difference of %d and %d is %d\n",a,b,d);
+	printf("product of %d and %d is %d\n",a,b,p);
+	printf("quotient of %d and %d is %d\n",a,b,q);
+	printf("reminder of %d and %d is %d\n",a,b,r);
+}
+
